a5ee9dd83f12b18f8503e86b40c55ef7a93501d9
Lucas Moretti <Lucas.morettic5@gmail.com>
2024-08-23 21:31:17 -0300

    primeiro commit

---
+++ 3506-csharpWeb-screensound-curso1-aula05-relacionamento/ScreenSound.API/Program.cs
@@ -0,0 +1,21 @@
+using ScreenSound.Banco;
+using ScreenSound.Modelos;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.
+    JsonOptions>(options => options.SerializerOptions.ReferenceHandler
+    = ReferenceHandler.IgnoreCycles);
+
+var app = builder.Build();
+
+
+
+app.MapGet("/", () =>
+{
+    var dal = new DAL<Artista>(new ScreenSoundContext());
+    return dal.Listar();
+});
+
+app.Run();
